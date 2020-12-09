fa3344ed625e37be62961e11c18f233aebb32e24
iamsoroush <soroush.moazed@gmail.com>
2020-12-09 11:15:36 +0330

    initial commit

--- apps/deepstream-test1/csiapp.py
+++ apps/deepstream-test1/csiapp.py
@@ -246,7 +246,7 @@ def main(args):
     # print("Playing file %s " % args[1])
     # source.set_property('location', args[1])
 
-    caps_nvvidconv_src.set_property('caps', Gst.Caps.from_string('video/x-raw(memory:NVMM), width=1280, height=720'))
+    caps_nvvidconv_src.set_property('caps', Gst.Caps.from_string('video/x-raw(memory:NVMM), sensor-id=0, width=1280, height=720'))
 
     streammux.set_property('width', 1280)
     streammux.set_property('height', 720)
