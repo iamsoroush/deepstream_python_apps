05a0bf098f9b18195769e81428465066f9f62390
iamsoroush <soroush.moazed@gmail.com>
2020-12-09 14:01:24 +0330

    added bitrate to camera config

--- apps/deepstream-test1/csiapp.py
+++ apps/deepstream-test1/csiapp.py
@@ -260,7 +260,7 @@ def main(args):
     # source.set_property('location', args[1])
 
     source.set_property('bufapi-version', True)
-    caps_nvvidconv_src.set_property('caps', Gst.Caps.from_string('video/x-raw(memory:NVMM), width=1280, height=720, sensor-id=0, format=NV12'))
+    caps_nvvidconv_src.set_property('caps', Gst.Caps.from_string('video/x-raw(memory:NVMM), width=1280, height=720, sensor-id=0'))
 
     streammux.set_property('width', 1280)
     streammux.set_property('height', 720)
